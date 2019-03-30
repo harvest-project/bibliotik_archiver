bd7dbfc59b79dabd6543dcff8f072a41c139d928
Harvest Project <admin@harvest-project.org>
2019-03-31 00:34:21 +0100

    Add num scraped in metadata task

--- models.py
+++ models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class BibliotikArchiverState(models.Model):
-    is_enabled = models.BooleanField()
+    is_metadata_enabled = models.BooleanField()
     is_download_enabled = models.BooleanField(default=False)
     last_meta_tracker_id = models.BigIntegerField()
--- tasks.py
+++ tasks.py
@@ -29,7 +29,7 @@ def bibliotik_archive_run():
     start = time.time()
 
     state = BibliotikArchiverState.objects.get()
-    if not state.is_enabled:
+    if not state.is_metadata_enabled:
         return
 
     client = BibliotikClient()
@@ -40,6 +40,7 @@ def bibliotik_archive_run():
 
     logger.info('Bibliotik max tracker id: {}.', max_tracker_id)
 
+    num_scraped = 0
     # last_meta_tracker_id was the last one processed, so resume from the next.
     for tracker_id in range(state.last_meta_tracker_id + 1, max_tracker_id):
         try:
@@ -49,6 +50,7 @@ def bibliotik_archive_run():
             logger.info('Bibliotik torrent {} not found.', tracker_id)
         state.last_meta_tracker_id = tracker_id
         state.save(update_fields=('last_meta_tracker_id',))
+        num_scraped += 1
         time.sleep(settings.BIBLIOTIK_ARCHIVER_METADATA_SLEEP)
         if time.time() - start >= 55:
             break
@@ -57,8 +59,8 @@ def bibliotik_archive_run():
     ComponentStatus.update_status(
         'bibliotik_archiver_metadata',
         ComponentStatus.STATUS_GREEN,
-        'Completed Bibliotik archiver metadata run in {:.3f} s. Progress: {} / {}.'.format(
-            time_taken, state.last_meta_tracker_id, max_tracker_id),
+        'Completed Bibliotik archiver metadata run with {} torrents in {:.3f} s. Progress: {} / {}.'.format(
+            num_scraped, time_taken, state.last_meta_tracker_id, max_tracker_id),
     )
 
 
@@ -83,7 +85,7 @@ def bibliotik_archive_download_torrent():
 
     tracker = TrackerRegistry.get_plugin('bibliotik', 'bibliotik_archive_download_torrent')
     realm = Realm.objects.get(name=tracker.name)
-    download_location = DownloadLocation.objects.filter(realm=realm).order_by('?').first()
+    download_location = realm.get_preferred_download_location()
 
     if not download_location:
         logger.error('No download location for realm {}.', tracker.name)
